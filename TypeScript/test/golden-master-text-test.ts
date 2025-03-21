e17ae25022ce3493d38f29242887f7367dfa432f
Raul Huelamo <raul@huelamo.dev>
2025-03-21 22:53:34 +0100

    test: add items into the golden master to cover missing use cases

--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -1,33 +1,50 @@
-import { Item, GildedRose } from '../app/gilded-rose';
+import { Item, GildedRose } from "../app/gilded-rose";
 
-console.log("OMGHAI!")
+console.log("OMGHAI!");
 
-const items = [
-  new Item("+5 Dexterity Vest", 10, 20), //
-  new Item("Aged Brie", 2, 0), //
-  new Item("Elixir of the Mongoose", 5, 7), //
-  new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-  // this conjured item does not work properly yet
-  new Item("Conjured Mana Cake", 3, 6)];
+// Items data for day 0
+const itemsData: Array<{ name: string; sellIn: number; quality: number }> = [
+  // Items provided by The Gilded Rose staff
+  { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
+  { name: "Aged Brie", sellIn: 2, quality: 0 },
+  { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
+  { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
+  { name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80 },
+  {
+    name: "Backstage passes to a TAFKAL80ETC concert",
+    sellIn: 15,
+    quality: 20,
+  },
+  {
+    name: "Backstage passes to a TAFKAL80ETC concert",
+    sellIn: 10,
+    quality: 49,
+  },
+  { name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49 },
 
+  // Items added by Dev team to cover missing edge cases
+  { name: "Sulfuras, Hand of Ragnaros", sellIn: 30, quality: 80 },
+
+  // TODO: New conjured items
+  // { name: "Conjured Mana Cake", sellIn: 3, quality: 6 },
+];
+
+const items = itemsData.map(
+  (item) => new Item(item.name, item.sellIn, item.quality)
+);
 
 const gildedRose = new GildedRose(items);
 
 let days: number = 2;
 if (process.argv.length > 2) {
-    days = +process.argv[2];
-  }
+  days = +process.argv[2];
+}
 
 for (let i = 0; i < days + 1; i++) {
   console.log("-------- day " + i + " --------");
   console.log("name, sellIn, quality");
-  items.forEach(element => {
-    console.log(element.name + ', ' + element.sellIn + ', ' + element.quality);
-
+  items.forEach((element) => {
+    console.log(element.name + ", " + element.sellIn + ", " + element.quality);
   });
   console.log();
   gildedRose.updateQuality();
