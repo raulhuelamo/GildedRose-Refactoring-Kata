4ed715f776192501a0e0d58161e81ba38c69e21a
Raul Huelamo <raul@huelamo.dev>
2025-03-30 20:58:32 +0200

    refactor: moar refactoring

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -10,6 +10,83 @@ export class Item {
   }
 }
 
+const ITEM_AGED_BRIE = "Aged Brie";
+const ITEM_BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+const ITEM_SULFURAS = "Sulfuras, Hand of Ragnaros";
+
+class GildedRoseItem {
+  item: Item;
+
+  minQuality: number = 0;
+  maxQuality: number = 50;
+
+  constructor(item: Item) {
+    this.item = item;
+  }
+
+  private increaseQuality() {
+    const isMaxQuality = this.item.quality < this.maxQuality;
+    if (!isMaxQuality) return;
+    this.item.quality++;
+  }
+
+  private decreaseQuality() {
+    const isMinQuality = this.item.quality > this.minQuality;
+    if (!isMinQuality) return;
+    this.item.quality--;
+  }
+
+  private dropQuality() {
+    this.item.quality = this.minQuality;
+  }
+
+  settleDay() {
+    const isSulfuras = this.item.name == ITEM_SULFURAS;
+    if (isSulfuras) return this.item;
+
+    this.item.sellIn--;
+
+    const isExpired = this.item.sellIn < 0;
+
+    if (this.item.name == ITEM_AGED_BRIE) {
+      this.increaseQuality();
+      if (isExpired) this.increaseQuality();
+      return this.item;
+    }
+
+    if (this.item.name == ITEM_BACKSTAGE_PASSES) {
+      if (isExpired) {
+        this.dropQuality();
+        return this.item;
+      }
+
+      const isRegularTicket = this.item.sellIn >= 5 && this.item.sellIn < 10;
+      const isLateBirdTicket = this.item.sellIn >= 0 && this.item.sellIn < 5;
+
+      const qualityIncreaseMultiplier = (() => {
+        if (isLateBirdTicket) return 3;
+        else if (isRegularTicket) return 2;
+        return 1;
+      })();
+
+      Array.from({ length: qualityIncreaseMultiplier }).forEach(() => {
+        this.increaseQuality();
+      });
+
+      return this.item;
+    }
+
+    const isCommon = true;
+    if (isCommon) {
+      this.decreaseQuality();
+      if (isExpired) this.decreaseQuality();
+      return this.item;
+    }
+
+    throw new Error("Unknown item type");
+  }
+}
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -18,60 +95,9 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
-
-    return this.items;
+    return this.items.map((_item) => {
+      const item = new GildedRoseItem(_item);
+      return item.settleDay();
+    });
   }
 }
